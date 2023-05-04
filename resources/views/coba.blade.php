445d015a820eaab67fde9ad7ecab3259455b9a79
anantaoktav <anantaoktav21@gmail.com>
2023-05-04 22:07:45 +0700

    test

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cobalur | coba</title>
+</head>
+<body>
+    <h1>Halaman coba</h1>
+    <h2>{{ $nama }}</h2>
+    <img src="image/{{ $image }}" alt="<?= $nama; ?>" width="200">
+</body>
+</html>
